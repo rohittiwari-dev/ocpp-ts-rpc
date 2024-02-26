1091bc1ec40106a37f2216facb035988eda9e2d0
Rohit Tiwari <tigertiwari1023@gmail.com>
2024-02-26 12:25:42 +0530

    [fix] url example

--- src/serverExample.ts
+++ src/serverExample.ts
@@ -65,6 +65,6 @@ server.listen(3000).then(() => {
 	console.log("🟢 Server Started at Port : ", 3000);
 	console.log("🟢 Websocket Url : ws://localhost:", 3000);
 	console.info(
-		"Just Add this url to your charger or charger simulator,\neg : ws://localhost:{port}/{chargerId}",
+		"Just Add this url to your charger or charger simulator,\neg : ws://localhost:{port}/{endpoint in any}/{chargerId}",
 	);
 });
